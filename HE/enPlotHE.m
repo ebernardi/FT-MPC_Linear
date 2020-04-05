783b044cf288bc23b67f2fd90254ebde0efc5c14
Emanuel <bernardiemanuel@gmail.com>
2020-04-05 09:53:03 -0300

    re-order files

--- HE/enPlotHE.m
+++ HE/enPlotHE.m
@@ -6,223 +6,152 @@ load FTCS
 vecrojo = [0.7; 0; 0]; vecverde = [0; 0.8; 0]; vecazul = [0; 0; 0.6]; negro = [.1; .1; .1]; gris = [.5; .7; .5];
 tc = 0:Ts/10:Nsim/20;
 
-for FT = 2:-1:1
-    % Outputs
-    figure(1)
-    if FT == 2
-        subplot(311)
-%         plot(t, state(1, :), 'b.', tc, y(1, :), 'y-.', t, xsp(1)*ones(length(t)), 'r--', 'LineWidth', 1.5);
-        plot(t, xsp(1)*ones(1, length(t)), 'r:', 'LineWidth', 1.5);
-        hold on
-%         plot(tc, Y_sim(1, :), 'b', 'LineWidth', 1.5);
-%         plot(t, Y(1, :), 'g:', t, Yfail(1, :), 'y-.', tc, Y_sim(1, :), 'b', 'LineWidth', 1.5);
-        plot(t, FTCS(FT).Y(1, :), 'b', t, FTCS(FT).Yfail(1, :), 'y-.', 'LineWidth', 1.5);
-        % plot(t, xmin(1)*ones(length(t)), 'y--')
-        % plot(t, xmax(1)*ones(length(t)), 'y--')
-        % plot(t, state(1, :), 'b-.', t, x1_hat(1, :), 'g:', t, x2_hat(1, :), 'y:', t, xsp(1)*ones(length(t)), 'r--', 'LineWidth', 1.5);
-        xlabel('Time [min]'); ylabel('\theta_1 [K]'); grid on
-        subplot(312)
-%         plot(t, state(2, :), 'b.', tc, y(2, :), 'y-.', t, xsp(2)*ones(length(t)), 'r--', 'LineWidth', 1.5)
-        plot(t, xsp(2)*ones(1, length(t)), 'r:', 'LineWidth', 1.5);
-        hold on
-%         h(2) = plot(tc, Y_sim(2, :), 'b', 'LineWidth', 1.5);
-        plot(t, FTCS(FT).Y(2, :), 'b', t, FTCS(FT).Yfail(2, :), 'y-.', 'LineWidth', 1.5);
-        legend('Actual-MPC', 'Measured-MPC', 'Location', 'NorthEast');
-%         plot(t, Y(2, :), 'g:', t, Yfail(2, :), 'y-.', tc, Y_sim(2, :), 'b', 'LineWidth', 1.5);
-        % plot(t, xmin(2)*ones(length(t)), 'y--')
-        % plot(t, xmax(2)*ones(length(t)), 'y--')
-        % plot(t, state(2, :), 'b-.', t, x1_hat(2, :), 'g:', t, x2_hat(2, :), 'y:', t, xsp(2)*ones(length(t)), 'r--', 'LineWidth', 1.5)
-        xlabel('Time [min]'); ylabel('\theta_2 [K]'); grid on
-        subplot(313)
-%         plot(t, state(3, :), 'b.', tc, y(3, :), 'y-.', t, xsp(3)*ones(length(t)), 'r--', 'LineWidth', 1.5)
-        plot(t, xsp(3)*ones(1, length(t)), 'r:', 'LineWidth', 1.5);
-        hold on
-%         plot(tc, Y_sim(3, :), 'b', 'LineWidth', 1.5);   
-        plot(t, FTCS(FT).Y(3, :), 'b', t, FTCS(FT).Yfail(3, :), 'y-.', 'LineWidth', 1.5);
-        % plot(t, xmin(3)*ones(length(t)), 'y--')
-        % plot(t, xmax(3)*ones(length(t)), 'y--')
-        % plot(t, state(3, :), 'b-.', t, x1_hat(3, :), 'g:', t, x2_hat(3, :), 'y:', t, xsp(3)*ones(length(t)), 'r--', 'LineWidth', 1.5)
-        xlabel('Time [min]'); ylabel('\theta_p [K]'); grid on
-    else
-        subplot(311)
-%         plot(t, state(1, :), 'k.', tc, y(1, :), 'g-.', 'LineWidth', 1.5);
-        plot(t, FTCS(FT).Y(1, :), 'k-.', t, FTCS(FT).Yfail(1, :), 'g:', 'LineWidth', 1.5);
-%         plot(t, Y(1, :), 'g:', t, Yfail(1, :), 'y-.', tc, Y_sim(1, :), 'b', 'LineWidth', 1.5);
-        hold off
-        legend('x_s', 'Location', 'SouthEast');
-        legend boxoff
-        subplot(312)
-%         plot(t, state(2, :), 'k.', tc, y(2, :), 'g-.', 'LineWidth', 1.5);
-        plot(t, FTCS(FT).Y(2, :), 'k-.', t, FTCS(FT).Yfail(2, :), 'g:', 'LineWidth', 1.5);
-%         plot(t, Y(2, :), 'g:', t, Yfail(2, :), 'y-.', tc, Y_sim(2, :), 'b', 'LineWidth', 1.5);
-        legend('', 'Actual-MPC', 'Measured-MPC', 'Location', 'SouthEast');
-        hold off
-%         legend(h(2), 'MPC', 'Location', 'SouthEast');
-        legend boxoff
-        subplot(313)
-%         plot(t, state(3, :), 'k.', tc, y(3, :), 'g-.', 'LineWidth', 1.5);
-        plot(t, FTCS(FT).Y(3, :), 'k-.', t, FTCS(FT).Yfail(3, :), 'g:', 'LineWidth', 1.5);
-%         h(3) = plot(t, Y(3, :), 'g:', t, Yfail(3, :), 'y-.', tc, Y_sim(3, :), 'b', 'LineWidth', 1.5);
-%         plot(t, Y(3, :), 'g:', t, Yfail(3, :), 'y-.', tc, Y_sim(3, :), 'b', 'LineWidth', 1.5);
-        hold off
-        legend('', '', '', 'Actual-FTMPC', 'Measured-FTMPC', 'Location', 'NorthEast');
-        legend boxoff
-        print -dsvg figs/outputHE.svg
-    end
+FTC_OFF = 1; FTC_ON = 2;
+%% Outputs
+figure(1)
 
-	%% Manipulated variables
-    figure(2)
-    if FT == 2
-        subplot(211)
-        stairs(t, FTCS(FT).U(1, 1:end), 'b', 'LineWidth', 1.5)
-        hold on
-        xlabel('Time [min]'); ylabel('q_1 [l/min]'); grid on
-        subplot(212)
-        stairs(t, FTCS(FT).U(2, 1:end), 'b', 'LineWidth', 1.5)
-        hold on
-        xlabel('Time [min]'); ylabel('q_2 [l/min]'); grid on
-	else
-        subplot(211)
-        stairs(t, FTCS(FT).U(1, 1:end), 'k-.', 'LineWidth', 1.5)
-        plot(t, FTCS(FT).Umin(1)*ones(length(t)), 'r--')
-        plot(t, FTCS(FT).Umax(1)*ones(length(t)), 'r--')
-        hold off
-        legend('MPC', 'FTMPC', 'Location', 'NorthWest');
-        legend boxoff
-        subplot(212)
-        stairs(t, FTCS(FT).U(2, 1:end), 'k-.', 'LineWidth', 1.5)
-        plot(t, FTCS(FT).Umin(2)*ones(length(t)), 'r--')
-        plot(t, FTCS(FT).Umax(2)*ones(length(t)), 'r--')
-        hold off
-        print -dsvg figs/inputHE.svg
-    end
+subplot(311)
+plot(t, xsp(1)*ones(1, length(t)), 'r:', 'LineWidth', 1.5);
+hold on; grid on
+plot(t, FTCS(FTC_OFF).Y(1, :), 'b', t, FTCS(FTC_OFF).Yfail(1, :), 'y-.', 'LineWidth', 1.5);
+plot(t, FTCS(FTC_ON).Y(1, :), 'k-.', t, FTCS(FTC_ON).Yfail(1, :), 'g:', 'LineWidth', 1.5);
+axis([0 inf 497 500]); hold off
+xlabel('Time [min]'); ylabel('\theta_1 [K]');
 
-%     % Failure inputs
-%     figure(3)
-%     if FTC == 0
-%         subplot(211)
-%         plot(t, FTCS(FTC).Umin(1, :), 'r--')
-%         hold on
-%         plot(t, FTCS(FTC).Umax(1, :), 'r--')
-%         stairs(t, Ufail(1, :), 'b', 'LineWidth', 1.5)
-%         xlabel('Time [min]'); ylabel('Q_1 [l/min]'); grid on
-%         subplot(212)
-%         plot(t, FTCS(FTC).Umin(2, :), 'r--')
-%         hold on
-%         plot(t, FTCS(FTC).Umax(2, :), 'r--')
-%         stairs(t, Ufail(2, :), 'b', 'LineWidth', 1.5)
-%         xlabel('Time [min]'); ylabel('Q_2 [l/min]'); grid on
-% 	else
-%         subplot(211)
-%         stairs(t, Ufail(1, :), 'k-.', 'LineWidth', 1.5)
-%         hold off
-%         subplot(212)
-%         stairs(t, Ufail(2, :), 'k-.', 'LineWidth', 1.5)
-%         hold off
-%         print -dsvg figs/inputfailHE.svg
-%     end
+subplot(312)
+plot(t, xsp(2)*ones(1, length(t)), 'r:', 'LineWidth', 1.5);
+hold on; grid on
+plot(t, FTCS(FTC_OFF).Y(2, :), 'b', t, FTCS(FTC_OFF).Yfail(2, :), 'y-.', 'LineWidth', 1.5);
+plot(t, FTCS(FTC_ON).Y(2, :), 'k-.', t, FTCS(FTC_ON).Yfail(2, :), 'g:', 'LineWidth', 1.5);
+axis([0 inf 688 692]); hold off
+xlabel('Time [min]'); ylabel('\theta_2 [K]');
 
-    % RUIO error detection
-    figure(4)
-    if FT == 2
-        subplot(211)
-        plot(t, FTCS(FT).RUIO(1).error, 'b', 'LineWidth', 1.5)
-        hold on; grid on
-        axis([0 inf 0 6.5])
-        subplot(212)
-        plot(t, FTCS(FT).RUIO(2).error, 'b', 'LineWidth', 1.5)
-        hold on; grid on
-        axis([0 inf 0 0.6])
-	else
-        subplot(211)
-        plot(t, FTCS(FT).RUIO(1).error, 'k-.', 'LineWidth', 1.5)
-        plot(t, threshold(1, :),  'r--', 'LineWidth', 1.5)
-        xlabel('Time [min]'); ylabel('|e_q|');
-        hold off
-        legend('MPC', 'FTMPC', 'Threshold', 'Location', 'NorthEast');
-        legend boxoff
-        subplot(212)
-        plot(t, FTCS(FT).RUIO(2).error, 'k-.', 'LineWidth', 1.5)
-        plot(t, threshold(2, :),  'r--', 'LineWidth', 1.5)
-        xlabel('Time [min]'); ylabel('|e_q|');
-        hold off
-        print -dsvg figs/RUIOerrorHE.svg
-    end
+subplot(313)
+plot(t, xsp(3)*ones(1, length(t)), 'r:', 'LineWidth', 1.5);
+hold on; grid on
+plot(t, FTCS(FTC_OFF).Y(3, :), 'b', t, FTCS(FTC_OFF).Yfail(3, :), 'y-.', 'LineWidth', 1.5);
+plot(t, FTCS(FTC_ON).Y(3, :), 'k-.', t, FTCS(FTC_ON).Yfail(3, :), 'g:', 'LineWidth', 1.5);
+axis([0 inf 557.4 558.4]);
+hold off
+xlabel('Time [min]'); ylabel('\theta_p [K]');
+legend('x_s', 'Actual-MPC', 'Measured-MPC', 'Actual-FTMPC', 'Measured-FTMPC', 'Location', 'NorthEast');
+legend boxoff
 
-	% UIOO error detection
-    figure(5)
-    if FT == 2
-        subplot(211)
-        plot(t, FTCS(FT).UIOO(1).error, 'b', 'LineWidth', 1.5)
-        hold on; grid on
-        subplot(212)
-        plot(t, UIOO(2).error, 'b', 'LineWidth', 1.5)
-        hold on; grid on
-    else
-        subplot(211)
-        plot(t, FTCS(FT).UIOO(1).error, 'k-.', 'LineWidth', 1.5)
-        plot(t, threshold(3, :),  'r--', 'LineWidth', 1.5)
-        axis([0 inf 0 0.04])
-        xlabel('Time [min]'); ylabel('|e_x|');
-        hold off
-        legend('MPC', 'FTMPC', 'Threshold', 'Location', 'NorthEast');
-        legend boxoff
-        subplot(212)
-        plot(t, FTCS(FT).UIOO(2).error, 'k-.', 'LineWidth', 1.5)
-        plot(t, threshold(4, :),  'r--', 'LineWidth', 1.5)
-        axis([0 inf 0 0.03])
-        xlabel('Time [min]'); ylabel('|e_x|');
-        hold off
-        print -dsvg figs/UIOOerrorHE.svg
-    end
+print -dsvg figs/outputHE.svg
 
-    % Fault estimation
-    figure(6)
-    if FT == 2
-        subplot(211)
-        stairs(t, FTCS(FT).RUIO(1).Fact, 'b', 'LineWidth', 1.5)
-        hold on
-        stairs(t, FTCS(FT).Ufail(1, :) - FTCS(FT).U(1, :), 'm--', 'LineWidth', 1.5)
-        xlabel('Time [min]'); ylabel('Q_1 [l/min]'); grid on
-        subplot(212)
-        stairs(t, FTCS(FT).RUIO(2).Fact, 'b', 'LineWidth', 1.5)
-        hold on
-        stairs(t, FTCS(FT).Ufail(2, :) - FTCS(FT).U(2, :), 'm--', 'LineWidth', 1.5)
-        xlabel('Time [min]'); ylabel('Q_2 [l/min]'); grid on
-    else
-        subplot(211)
-        stairs(t, FTCS(FT).RUIO(1).Fact, 'k-.', 'LineWidth', 1.5)
-        hold off
-        subplot(212)
-        stairs(t, FTCS(FT).RUIO(2).Fact, 'k-.', 'LineWidth', 1.5)
-        hold off
-        print -dsvg figs/actuatorEstimationHE.svg
-    end
+%% Manipulated variables (with actuator faults)
+figure(2)
+subplot(211)
+stairs(t, FTCS(FTC_OFF).Ufail(1, 1:end), 'b', 'LineWidth', 1.5)
+hold on; grid on
+stairs(t, FTCS(FTC_ON).Ufail(1, 1:end), 'k-.', 'LineWidth', 1.5)
+stairs(t, FTCS(FTC_OFF).Umin(1, 1:end), 'r--', 'LineWidth', 1.5)
+stairs(t, FTCS(FTC_OFF).Umax(1, 1:end), 'r--', 'LineWidth', 1.5)
+hold off
+xlabel('Time [min]'); ylabel('q_1 [l/min]');
+legend('MPC', 'FTMPC', 'Location', 'NorthWest');
+legend boxoff
 
-    % Fault estimation
-    figure(7)
-    if FT == 2
-        subplot(211)
-        stairs(t, FTCS(FT).UIOO(1).Fsen, 'b', 'LineWidth', 1.5)
-        hold on
-        stairs(t, FTCS(FT).Yfail(1, :) - FTCS(FT).Y(1, :), 'm--', 'LineWidth', 1.5)
-        xlabel('Time [min]'); ylabel('\theta_1 [K]'); grid on
-        subplot(212)
-        stairs(t, FTCS(FT).UIOO(2).Fsen, 'b', 'LineWidth', 1.5)
-        hold on
-        stairs(t, FTCS(FT).Yfail(2, :) - FTCS(FT).Y(2, :), 'm--', 'LineWidth', 1.5)
-        xlabel('Time [min]'); ylabel('\theta_2 [K]'); grid on
-    else
-        subplot(211)
-        stairs(t, FTCS(FT).UIOO(1).Fsen, 'k-.', 'LineWidth', 1.5)
-        hold off
-        subplot(212)
-        stairs(t, FTCS(FT).UIOO(2).Fsen, 'k-.', 'LineWidth', 1.5)
-        hold off
-        print -dsvg figs/sensorEstimationHE.svg
-    end
-    
-%     % Objective
+subplot(212)
+stairs(t, FTCS(FTC_OFF).Ufail(2, 1:end), 'b', 'LineWidth', 1.5)
+hold on; grid on
+stairs(t, FTCS(FTC_ON).Ufail(2, 1:end), 'k-.', 'LineWidth', 1.5)
+stairs(t, FTCS(FTC_OFF).Umin(2, 1:end), 'r--', 'LineWidth', 1.5)
+stairs(t, FTCS(FTC_OFF).Umax(2, 1:end), 'r--', 'LineWidth', 1.5)
+hold off
+xlabel('Time [min]'); ylabel('q_2 [l/min]');
+
+print -dsvg figs/inputHE.svg
+
+%% RUIO error detection
+figure(4)
+subplot(211)
+plot(t, FTCS(FTC_OFF).RUIO(1).error, 'b', 'LineWidth', 1.5)
+hold on; grid on
+plot(t, FTCS(FTC_ON).RUIO(1).error, 'k-.', 'LineWidth', 1.5)
+plot(t, threshold(1, :),  'r--', 'LineWidth', 1.5)
+hold off
+axis([0 inf 0 6.5])
+xlabel('Time [min]'); ylabel('|e_q|');
+legend('MPC', 'FTMPC', 'Threshold', 'Location', 'NorthEast');
+legend boxoff
+
+subplot(212)
+plot(t, FTCS(FTC_OFF).RUIO(2).error, 'b', 'LineWidth', 1.5)
+hold on; grid on
+plot(t, FTCS(FTC_ON).RUIO(2).error, 'k-.', 'LineWidth', 1.5)
+plot(t, threshold(2, :),  'r--', 'LineWidth', 1.5)
+hold off
+axis([0 inf 0 0.6])
+xlabel('Time [min]'); ylabel('|e_q|');
+
+print -dsvg figs/RUIOerrorHE.svg
+
+%% UIOO error detection
+figure(5)
+subplot(211)
+plot(t, FTCS(FTC_OFF).UIOO(1).error, 'b', 'LineWidth', 1.5)
+hold on; grid on
+plot(t, FTCS(FTC_OFF).UIOO(1).error, 'k-.', 'LineWidth', 1.5)
+plot(t, threshold(3, :),  'r--', 'LineWidth', 1.5)
+hold off
+axis([0 inf 0 0.04])
+xlabel('Time [min]'); ylabel('|e_x|');
+legend('MPC', 'FTMPC', 'Threshold', 'Location', 'NorthEast');
+legend boxoff
+
+subplot(212)
+plot(t, FTCS(FTC_ON).UIOO(2).error, 'b', 'LineWidth', 1.5)
+hold on; grid on
+plot(t, FTCS(FTC_ON).UIOO(2).error, 'k-.', 'LineWidth', 1.5)
+plot(t, threshold(4, :),  'r--', 'LineWidth', 1.5)
+hold off
+axis([0 inf 0 0.03])
+xlabel('Time [min]'); ylabel('|e_x|');
+
+print -dsvg figs/UIOOerrorHE.svg
+
+%% Actuator fault estimation
+figure(6)
+subplot(211)
+stairs(t, FTCS(FTC_OFF).RUIO(1).Fact, 'b', 'LineWidth', 1.5)
+hold on; grid on
+stairs(t, FTCS(FTC_OFF).Ufail(1, :) - FTCS(FTC_OFF).U(1, :), 'm--', 'LineWidth', 1.5)
+stairs(t, FTCS(FTC_ON).RUIO(1).Fact, 'k-.', 'LineWidth', 1.5)
+hold off
+xlabel('Time [min]'); ylabel('Q_1 [l/min]');
+
+subplot(212)
+stairs(t, FTCS(FTC_OFF).RUIO(2).Fact, 'b', 'LineWidth', 1.5)
+hold on; grid on
+stairs(t, FTCS(FTC_OFF).Ufail(2, :) - FTCS(FTC_OFF).U(2, :), 'm--', 'LineWidth', 1.5)
+stairs(t, FTCS(FTC_ON).RUIO(2).Fact, 'k-.', 'LineWidth', 1.5)
+hold off
+xlabel('Time [min]'); ylabel('Q_2 [l/min]');
+
+print -dsvg figs/actuatorEstimationHE.svg
+
+%% Sensor fault estimation
+figure(7)
+subplot(211)
+stairs(t, FTCS(FTC_OFF).UIOO(1).Fsen, 'b', 'LineWidth', 1.5)
+hold on; grid on
+stairs(t, FTCS(FTC_OFF).Yfail(1, :) - FTCS(FTC_OFF).Y(1, :), 'm--', 'LineWidth', 1.5)
+stairs(t, FTCS(FTC_ON).UIOO(1).Fsen, 'k-.', 'LineWidth', 1.5)
+hold off
+xlabel('Time [min]'); ylabel('\theta_1 [K]');
+
+subplot(212)
+stairs(t, FTCS(FTC_OFF).UIOO(2).Fsen, 'b', 'LineWidth', 1.5)
+hold on; grid on
+stairs(t, FTCS(FTC_OFF).Yfail(2, :) - FTCS(FTC_OFF).Y(2, :), 'm--', 'LineWidth', 1.5)
+stairs(t, FTCS(FTC_ON).UIOO(2).Fsen, 'k-.', 'LineWidth', 1.5)
+hold off
+xlabel('Time [min]'); ylabel('\theta_2 [K]');
+
+print -dsvg figs/sensorEstimationHE.svg
+
+%%     % Objective
 %     figure(8)
 %     if FTC == 0
 %         plot(t, Obj, 'b', 'LineWidth', 1.5)
@@ -323,4 +252,3 @@ for FT = 2:-1:1
 %         hold off
 %         print -dsvg figs/state2-3HE.svg    
 %     end
-end
--- HE/simHE.m
+++ HE/simHE.m
@@ -37,8 +37,8 @@ Fail_Q1 = 5; Fail_Q2 = 0.43;    % Actuator fault magnitude [5%, 5%]
 Fail_S1 = 1.5; Fail_S2 = -1.5;	% Sensor fault magnitude [0.5% 0.5%]
 x0 = [495; 680; 570];             % Start-point
 xsp = [Theta_1s; Theta_2s; Theta_p];% Set-point
-Np = 5;                                   % Prediction horizon
 
+%% MPC controller
 % Constraints
 xmin = [495; 650; 530];
 xmax = [500; 750; 590];
@@ -51,6 +51,10 @@ Qx = eye(nx);
 % Rx = eye(nu);
 Rx = diag([1 0.1]);
 gamma = 1e2*diag([1e6 1 1]); 
+
+% Controller
+Np = 5;                                   % Prediction horizon
+run MPC;
 
 %% Noise
 sig = 3e-3*([1 1 1])';         % Ouput noise sigma
@@ -96,11 +100,8 @@ for k = 1:Nsim
     threshold(4, k) = mag_4 + 1000*exp(-(k-1)/Tau);  % O2
 end
 
-%% MPC controller
-run MPC;
-
 %% Simulation Setup
-% Fault Tolerant Control System (1 = enable, 2 = disable).Matrix 
+% Fault Tolerant Control System (1 = disable; 2 = enable).Matrix 
 FTCS = struct;
 for j = 1:2
     FTCS(j).U = zeros(nu, Nsim);                   % Control Input
@@ -145,34 +146,30 @@ for j = 1:2
 end
               
 %% Simulation
-for FT = 2:-1:1 % 1 - FT is on; 2 - FT is off
+FTC_OFF = 1; FTC_ON = 2;
+for FT = 1:2    % 1 - FT is off; 2 -  FT is on
     
     % Vector initialization for plots
-    FTCS(FT).Y_sim = C*x0; FTCS(FT).Umin = umin; FTCS(FT).Umax = umax;
+    FTCS(FT).Yc_sim = C*x0;
     
     for k = 1:Nsim
         tk = k*Ts; % Simulation time
-        
-        if FT == 1
-            if k == 1
-                YY = FTCS(FT).X(:, k);
-                UF = FTCS(FT).Uff(:, k);
-            else
-                YY = [FTCS(FT).Yfail(1, k-1)-UIOO(1).Fsen(k); FTCS(FT).Yfail(2, k-1)-UIOO(2).Fsen(k); FTCS(FT).Yfail(3, k-1)];
-                UF = FTCS(FT).Uff(:, k);
-            end
+
+        if k == 1
+            yMPC = C*FTCS(FT).X(:, k);
+            uffMPC = FTCS(FT).Uff(:, k);
         else
-            if k == 1
-                YY = FTCS(FT).X(:, k);
-                UF = FTCS(FT).Uff(:, k);
+            if FT == FTC_ON
+                yMPC = [FTCS(FT).Yfail(1, k-1)-UIOO(1).Fsen(k); FTCS(FT).Yfail(2, k-1)-UIOO(2).Fsen(k); FTCS(FT).Yfail(3, k-1)];
+                uffMPC = FTCS(FT).Uff(:, k);
             else
-                YY = FTCS(FT).Yfail(:, k-1);%X(:, k);%
-                UF = FTCS(FT).Uff(:, k);
+                yMPC = FTCS(FT).Yfail(:, k-1);
+                uffMPC = FTCS(FT).Uff(:, k);
             end       
         end
         
         % Run MPC controller
-        [sol, diag] = mpc{YY, xsp, UF};
+        [sol, diag] = mpc{yMPC, xsp, uffMPC};
         if diag
             msg = ['Infeasible problem at t = ', num2str(k*Ts)];
             disp(msg)
@@ -181,31 +178,27 @@ for FT = 2:-1:1 % 1 - FT is on; 2 - FT is off
         FTCS(FT).U(:, k) = sol{1}; FTCS(FT).Obj(:, k) = sol{2};
 
         %% Actuator fault income
-%         if k*Ts > 5 && k*Ts < 15
-%             Ufail(:, k) = U(:, k) + f1;
-%             FTCS(FTC).Umax(:, k) = FTCS(FTC).Umax + f1;
-%             FTCS(FTC).Umin(:, k) = FTCS(FTC).Umin + f1;
-%         elseif k*Ts > 20 && k*Ts < 25
-%             Ufail(:, k) = U(:, k) + f2;
-%             FTCS(FTC).Umax(:, k) = FTCS(FTC).Umax + f2;
-%             FTCS(FTC).Umin(:, k) = FTCS(FTC).Umin + f2;
-%         else
-%             Ufail(:, k) = U(:, k);
-%         end
-
-        FTCS(FT).Ufail(:, k) = FTCS(FT).U(:, k);      
+        % No fault
         FTCS(FT).Ufails(:, k) = [0; 0];
+        FTCS(FT).Ufail(:, k) = FTCS(FT).U(:, k);      
         FTCS(FT).Umax(:, k) = umax;
         FTCS(FT).Umin(:, k) = umin;
  
+        % Q1 fault
         if tk > 5 && tk < 15
             FTCS(FT).Ufails(:, k) = [Fail_Q1; 0];
             FTCS(FT).Ufail(:, k) = FTCS(FT).U(:, k) + FTCS(FT).Ufails(:, k);
+            % TODO: Check if the constraints may be modified
+%             FTCS(FT).Umax(:, k) = umax + FTCS(FT).Ufails(:, k);
+%             FTCS(FT).Umin(:, k) = umin + FTCS(FT).Ufails(:, k);
         end
 
+        % Q2 fault
         if tk > 20 && tk < 25
             FTCS(FT).Ufails(:, k) = [0; -Fail_Q2+Fail_Q2*(exp(-2*(tk-20)/1))];
             FTCS(FT).Ufail(:, k) = FTCS(FT).U(:, k) + FTCS(FT).Ufails(:, k);
+%             FTCS(FT).Umax(:, k) = umax + FTCS(FT).Ufails(:, k);
+%             FTCS(FT).Umin(:, k) = umin + FTCS(FT).Ufails(:, k);
         end
 
         % Natural system saturation
@@ -221,10 +214,10 @@ for FT = 2:-1:1 % 1 - FT is on; 2 - FT is off
         %TODO: Evaluate use ODE tool
         Dt  = linspace(0, Ts, 10)';
         u = (FTCS(FT).Ufail(:, k) - U_lin)*ones(1, numel(Dt)); % Zero-order hold input (with Input offset)
-        X_sim = lsim(sys, u, Dt, FTCS(FT).X(:, k)-X_lin);   % Nominal continuous system
-        X_sim = X_sim' + X_lin;                        % (with state offset)
-        FTCS(FT).Y_sim = [FTCS(FT).Y_sim C*X_sim];                    % System output with offset (Continuous time)
-        FTCS(FT).X(:, k+1) = X_sim(:, end);                     % Discrete-time state
+        Xc_sim = lsim(sys, u, Dt, FTCS(FT).X(:, k)-X_lin);   % Nominal continuous system
+        Xc_sim = Xc_sim' + X_lin;                                      % (with state offset)
+        FTCS(FT).Yc_sim = [FTCS(FT).Yc_sim C*Xc_sim];  % System output with offset (Continuous time)
+        FTCS(FT).X(:, k+1) = Xc_sim(:, end) + v(:, k);       % Discrete-time state + noise
         
         % Natural state limits (maybe should be erased)
         for j = 1:nu
@@ -235,20 +228,11 @@ for FT = 2:-1:1 % 1 - FT is on; 2 - FT is off
             end
         end
         
-        FTCS(FT).Y(:, k) = C*FTCS(FT).X(:, k);                                  % Discrete-time output
+        FTCS(FT).Y(:, k) = C*FTCS(FT).X(:, k);                   % Discrete-time output
 
         %% Sensor fault income
-        %TODO: Add output fails
         FTCS(FT).Yfail(:, k) = FTCS(FT).Y(:, k);
         
-%         if k*Ts > 28 && k*Ts < 38
-%             Yfail(:, k) = Y(:, k) + [0; -3.5; 0];
-%         end
-% 
-%         if k*Ts >40 && k*Ts < 50
-%             Yfail(:, k) = Y(:, k) + [3; 0; 0];
-%         end
-
         if tk > 28 && tk < 38
             FTCS(FT).Yfail(:, k) = FTCS(FT).Y(:, k) + [0; Fail_S2-Fail_S2*(exp(-5*(tk-28)/1)); 0];
         end
@@ -367,7 +351,7 @@ for FT = 2:-1:1 % 1 - FT is on; 2 - FT is off
         end
         
         % If FT-MPC is enabled
-        if FT == 1
+        if FT == FTC_ON
             FTCS(FT).Uff(:, k+1) = [RUIO(1).Fact(k); RUIO(2).Fact(k)];
         else
             FTCS(FT).Uff(:, k+1) = [0; 0];
